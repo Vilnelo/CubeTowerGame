f68fe50a13267916786a5b25c440ceca23c49351
denis.egorov <denis.e@danko.team>
2025-07-31 02:07:36 +0700

    Добавление нижней панели с блоками

---
+++ Assets/Core/BottomBlocks/External/BottomBlockView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.BottomBlocks.External
+{
+    public class BottomBlockView : MonoBehaviour
+    {
+        [SerializeField] private Image m_BlockSprite;
+
+        public void SetImage(Sprite sprite)
+        {
+            m_BlockSprite.sprite = sprite;
+        }
+    }
+}
